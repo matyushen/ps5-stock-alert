f0952e10cc93edacafac9bdf91dd2074ad275145
Nikita Matyushenko <matyushen@users.noreply.github.com>
2020-12-24 16:54:02 +0100

    feat: notify if no twilio credentials were provided

--- src/sendMessage.ts
+++ src/sendMessage.ts
@@ -5,9 +5,11 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
 
 export const sendMessage = async (link: Link) => {
-  console.log(
-    `Sending a stock alert to Whatsapp ${process.env.TWILIO_PHONE_TO}`
-  );
+  if (!accountSid && !authToken) {
+    console.log(`No Twilio credentials provided. Skipping sending a message.`);
+    return;
+  }
+  console.log(`Sending a stock alert SMS to ${process.env.TWILIO_PHONE_TO}`);
 
   await client.messages.create({
     from: `${process.env.TWILIO_PHONE_FROM}`,
