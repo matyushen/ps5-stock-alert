c2b86a363cd881bc17134c32d4cad07378010e66
Nikita Matyushenko <matyushen@users.noreply.github.com>
2020-12-25 08:10:46 +0100

    feat: move to express server with cron job

---
+++ src/checkPages.ts
@@ -0,0 +1,108 @@
+import { links, Link, LinkType } from "./links";
+import { ChromiumBrowserContext } from "playwright";
+import { playSiren } from "./play";
+import { sendMessage } from "./sendMessage";
+import { format } from "date-fns";
+import { Page } from "playwright/types/types";
+
+const { chromium } = require("playwright");
+
+const sleep = (ms: number) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+
+const handleStockAvailability = async (
+  link: Link,
+  stockFound: boolean,
+  page: Page
+) => {
+  if (!stockFound) {
+    console.log(`Still no stock for ${link.name}: ${link.url}`);
+    return;
+  }
+  console.log(`🚨 ${" "}There might be a ${link.name} in stock at ${link.url}`);
+  await page.screenshot({
+    path: `src/assets/screenshot-${format(new Date(), "PPpp")}.png`,
+  });
+  await sendMessage(link);
+  await playSiren();
+};
+
+const evaluateAmazonLink = async (
+  link: Link,
+  context: ChromiumBrowserContext
+) => {
+  const page = await context.newPage();
+  await page.goto(link.url);
+  if (link.dataDefaultAsin) {
+    const variantButton = await page.$(
+      `li[data-defaultasin=${link.dataDefaultAsin}] button`
+    );
+
+    if (variantButton) {
+      await variantButton.click({ force: true });
+      // FIXME: Next assertion is done before page reload for some reason, so we wait
+      await sleep(1000);
+    }
+  }
+
+  const addToCartButton = await page.$(
+    "#desktop_buybox_feature_div #addToCart input#add-to-cart-button"
+  );
+
+  await handleStockAvailability(link, !!addToCartButton, page);
+
+  await page.close();
+};
+
+const evaluateMediaMarktLink = async (
+  link: Link,
+  context: ChromiumBrowserContext
+) => {
+  const page = await context.newPage();
+  await page.goto(link.url);
+  const title = await page.textContent('[data-test="product-title"]');
+  await handleStockAvailability(
+    link,
+    title.includes("SONY PlayStation 5"),
+    page
+  );
+  await page.close();
+};
+
+const evaluateGameStopLink = async (
+  link: Link,
+  context: ChromiumBrowserContext
+) => {
+  const page = await context.newPage();
+  await page.goto(link.url);
+  const sorryTitle = await page.$('text="Sorry, PS5-Fans."');
+  await handleStockAvailability(link, !sorryTitle, page);
+  await page.close();
+};
+
+export const checkPages = async () => {
+  const browser = await chromium.launch({ headless: true });
+  const browserContext = await browser.newContext({
+    viewport: {
+      width: 2560,
+      height: 1440,
+      deviceScaleFactor: 2,
+    },
+  });
+
+  for (const link of links) {
+    if (link.type === LinkType.AMAZON) {
+      await evaluateAmazonLink(link, browserContext);
+    }
+    if (link.type === LinkType.MEDIAMARKT) {
+      await evaluateMediaMarktLink(link, browserContext);
+    }
+    if (link.type === LinkType.GAMESTOP) {
+      await evaluateGameStopLink(link, browserContext);
+    }
+  }
+
+  await browser.close();
+};
--- src/index.ts
+++ src/index.ts
@@ -1,109 +1,22 @@
-import { links, Link, LinkType } from "./links";
-import { ChromiumBrowserContext } from "playwright";
+import { checkPages } from "./checkPages";
 import { format } from "date-fns";
-import { playSiren } from "./play";
-import { sendMessage } from "./sendMessage";
 
-const { chromium } = require("playwright");
-const TIMEOUT = 5 * 60 * 1000;
+const express = require("express");
+const cron = require("node-cron");
+const app = express();
 
-const sleep = (ms: number) =>
-  new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+let count = 1;
 
-const handleStockAvailability = async (link: Link, stockFound: boolean) => {
-  if (!stockFound) {
-    console.log(`Still no stock for ${link.name}: ${link.url}`);
-    return;
-  }
-  console.log(`🚨 ${" "}There might be a ${link.name} in stock at ${link.url}`);
-  await sendMessage(link);
-  await playSiren();
-};
-
-const evaluateAmazonLink = async (
-  link: Link,
-  context: ChromiumBrowserContext
-) => {
-  const page = await context.newPage();
-  await page.goto(link.url);
-  if (link.dataDefaultAsin) {
-    const variantButton = await page.$(
-      `li[data-defaultasin=${link.dataDefaultAsin}] button`
-    );
-
-    if (variantButton) {
-      await variantButton.click({ force: true });
-      // FIXME: Next assertion is done before page reload for some reason, so we wait
-      await sleep(1000);
-    }
-  }
-
-  const addToCartButton = await page.$(
-    "#desktop_buybox_feature_div #addToCart input#add-to-cart-button"
-  );
-
-  await handleStockAvailability(link, !!addToCartButton);
-
-  await page.close();
-};
-
-const evaluateMediaMarktLink = async (
-  link: Link,
-  context: ChromiumBrowserContext
-) => {
-  const page = await context.newPage();
-  await page.goto(link.url);
-  const title = await page.textContent('[data-test="product-title"]');
-  await handleStockAvailability(link, title.includes("SONY PlayStation 5"));
-  await page.close();
-};
-
-const evaluateGameStopLink = async (
-  link: Link,
-  context: ChromiumBrowserContext
-) => {
-  const page = await context.newPage();
-  await page.goto(link.url);
-  const sorryTitle = await page.$('text="Sorry, PS5-Fans."');
-  await handleStockAvailability(link, !sorryTitle);
-  await page.close();
-};
-
-const run = async () => {
-  const browser = await chromium.launch({ headless: true });
-  const browserContext = await browser.newContext({
-    viewport: {
-      width: 2560,
-      height: 1440,
-      deviceScaleFactor: 2,
-    },
-  });
-  let count = 1;
-
-  const checkPages = async () => {
-    console.log(`🚀 ${" "}Cycle: #${count}`);
-    for (let link of links) {
-      if (link.type === LinkType.AMAZON) {
-        await evaluateAmazonLink(link, browserContext);
-      }
-      if (link.type === LinkType.MEDIAMARKT) {
-        await evaluateMediaMarktLink(link, browserContext);
-      }
-      if (link.type === LinkType.GAMESTOP) {
-        await evaluateGameStopLink(link, browserContext);
-      }
-    }
-    console.log(`💤 ${" "}Sleeping at ${format(new Date(), "PPpp")}`);
-    count += 1;
-    await sleep(TIMEOUT);
-    await checkPages();
-  };
-
+const task = cron.schedule("*/5 * * * *", async () => {
+  console.log(`🚀 ${" "} Running a #${count} cycle`);
   await checkPages();
+  count += 1;
+  console.log(`💤 ${" "}Sleeping at ${format(new Date(), "PPpp")}`);
+});
 
-  await browser.close();
-};
+app.get("/", (req: any, res: any) => {
+  res.send("Hello World");
+  task.start();
+});
 
-run().catch((e) => console.log(e.message));
+app.listen(3000);
