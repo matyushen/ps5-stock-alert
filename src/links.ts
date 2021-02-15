ae16fbb264498405a44f9057ef1f228e12a40525
Nikita Matyushenko <matyushen@users.noreply.github.com>
2021-02-15 21:46:50 +0100

    fix: correct id

--- src/links.ts
+++ src/links.ts
@@ -50,7 +50,7 @@ export const links: Link[] = [
   {
     name: "Amazon DE - Digital",
     url: "https://www.amazon.de/-/en/dp/B08H98GVK8",
-    dataDefaultAsin: "B08H93ZRK9",
+    dataDefaultAsin: "B08H98GVK8",
     type: LinkType.AMAZON,
   },
   {
