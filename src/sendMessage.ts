7f7424275370c5452c00643ac752f1a2dd011c54
Nikita Matyushenko <matyushen@users.noreply.github.com>
2021-06-02 09:12:35 +0200

    feat: add slack

--- src/sendMessage.ts
+++ src/sendMessage.ts
@@ -3,10 +3,30 @@ import { createReadStream } from "fs";
 import { Page } from "playwright";
 import { getEnvVar } from "./utils";
 const telegram = require("telegram-bot-api");
+import { WebClient } from "@slack/client";
 
 const client = new telegram({
   token: getEnvVar("TELEGRAM_BOT_TOKEN"),
 });
+
+const sendSlackMessage = async (text: string): Promise<void> => {
+  const token = process.env.SLACK_TOKEN;
+  const channel = process.env.SLACK_CHANNEL_ID;
+
+  if (!token || !channel) return;
+
+  const slackClient = new WebClient(token);
+  try {
+    await slackClient.chat.postMessage({
+      channel,
+      text: `${text} <!channel>`,
+      unfurl_links: false,
+      unfurl_media: false,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 export const sendMessage = async (
   message: string,
@@ -16,6 +36,8 @@ export const sendMessage = async (
   await page.screenshot({
     path,
   });
+
+  await sendSlackMessage(message);
 
   client
     .sendPhoto({
