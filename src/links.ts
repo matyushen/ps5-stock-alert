1ee02ae6237a9811b7f2cc043a4e1d3bc2901f9e
Nikita Matyushenko <matyushen@users.noreply.github.com>
2021-09-07 06:25:08 +0200

    Update links.ts

--- src/links.ts
+++ src/links.ts
@@ -53,13 +53,13 @@ export const links: Link[] = [
     dataDefaultAsin: "B08H98GVK8",
     type: LinkType.AMAZON,
   },
-  {
-    name: "Amazon IT - Digital",
-    url:
-      "https://www.amazon.it/Sony-PlayStation-5-Digital-Edition/dp/B08KJF2D25",
-    dataDefaultAsin: "B08KJF2D25",
-    type: LinkType.AMAZON,
-  },
+//   {
+//     name: "Amazon IT - Digital",
+//     url:
+//       "https://www.amazon.it/Sony-PlayStation-5-Digital-Edition/dp/B08KJF2D25",
+//     dataDefaultAsin: "B08KJF2D25",
+//     type: LinkType.AMAZON,
+//   },
   {
     name: "Amazon NL - Digital",
     url:
@@ -79,6 +79,67 @@ export const links: Link[] = [
     url:
       "https://www.amazon.es/Playstation-Consola-PlayStation-5/dp/B08KJF2D25",
     dataDefaultAsin: "B08KJF2D25",
+    type: LinkType.AMAZON,
+  },
+  {
+    name: "Amazon ES - Consola PlayStation 5 + Marvel´s Spider-Man Miles Morales + Destruction Allstars",
+    url: "https://www.amazon.es/Playstation-Consola-PlayStation-5/dp/B095WS48MB",
+    dataDefaultAsin: "B095WS48MB",
+    type: LinkType.AMAZON,
+  },
+    {
+    name: "PlayStation 5 Consola Digital + Mando inalámbrico DualSense",
+    url:
+      "https://www.amazon.es/Playstation-Consola-PlayStation-5/dp/B08X1WG2GJ",
+    dataDefaultAsin: "B08X1WG2GJ",
+    type: LinkType.AMAZON,
+  },
+    {
+    name: "PlayStation 5 - PS5 Standard Ratchet & Clank + Suscripción PlayStation Plus 365 + Destruction AllStars [Bundle]",
+    url: "https://www.amazon.es/Playstation-Consola-PlayStation-5/dp/B097FHC3VB",
+    dataDefaultAsin: "B097FHC3VB",
+    type: LinkType.AMAZON,
+  },
+    {
+    name: "PlayStation 5 - PS5 Standard + Ratchet & Clank + Tarjeta PSN 20 € [Bundle]",
+    url: "https://www.amazon.es/Playstation-Consola-PlayStation-5/dp/B0977DGMX7",
+    dataDefaultAsin: "B0977DGMX7",
+    type: LinkType.AMAZON,
+  },
+    {
+    name: "Sony Consola PS5 & Ratchet & Clank - Una Dimensión Aparte",
+    url: "https://www.amazon.es/Playstation-Consola-PlayStation-5/dp/B099BQFGJL",
+    dataDefaultAsin: "B099BQFGJL",
+    type: LinkType.AMAZON,
+  },
+    {
+    name: "Consola PlayStation 5 + Mando inalámbrico DualSense Cosmic Red",
+    url: "https://www.amazon.es/Playstation-Consola-PlayStation-5/dp/B09CZ9JWD9",
+    dataDefaultAsin: "B09CZ9JWD9",
+    type: LinkType.AMAZON,
+  },
+    {
+    name: "Consola PlayStation 5 + Mando inalámbrico DualSense Midnight Black",
+    url: "https://www.amazon.es/Playstation-Consola-PlayStation-5/dp/B09CZ9P2FK",
+    dataDefaultAsin: "B09CZ9P2FK",
+    type: LinkType.AMAZON,
+  },
+    {
+    name: "PlayStation 5 Consola + Mando inalámbrico DualSense",
+    url: "https://www.amazon.es/Playstation-Consola-PlayStation-5/dp/B08X1SPNKR",
+    dataDefaultAsin: "B08X1SPNKR",
+    type: LinkType.AMAZON,
+  },
+    {
+    name: "PlayStation 5 - PS5 825GB Digital + Mando Media Remote PS5 + Tarjeta Suscripción PlayStation Plus 365 [Bundle]",
+    url: "https://www.amazon.es/Playstation-Consola-PlayStation-5/dp/B097FHCM3J",
+    dataDefaultAsin: "B097FHCM3J",
+    type: LinkType.AMAZON,
+  },
+    {
+    name: "Consola PlayStation 5 Digital + PlayStation 5 - Mando inalámbrico DualSense Midnight Black - Exclusivo para PS5",
+    url: "https://www.amazon.es/Playstation-Consola-PlayStation-5/dp/B097J2YHBG",
+    dataDefaultAsin: "B097J2YHBG",
     type: LinkType.AMAZON,
   },
   {
